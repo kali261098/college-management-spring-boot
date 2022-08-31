e1c9a5f2c2aacf9d84e28d90476a61aca6b22c18
mariappanj203 <79914060+mariappanj203@users.noreply.github.com>
2022-08-31 13:08:56 +0530

    twillio

---
+++ src/main/java/com/example/demo/controller/SmsController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.twilio.Twilio;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@CrossOrigin
+@RestController
+public class SmsController {
+
+    @GetMapping(value = "/sendSMS")
+    public ResponseEntity<String> sendSMS() {
+        String sid = "ACb6b29e0f9eefefafd9b07ced38ee4276";
+        String token = "ac4b40e68791b0bf7b040201b6ac7a9d";
+
+        Twilio.init(System.getenv(sid), System.getenv(token));
+
+        Message.creator(new PhoneNumber("+919786161674"),
+                new PhoneNumber("+919626978553"), "Hello from Twilio 📞").create();
+
+        return new ResponseEntity<String>("Message sent successfully", HttpStatus.OK);
+    }
+}
